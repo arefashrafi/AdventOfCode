7cf7ca33dac46940788f4cf7936a86af15b77a32
Aref Ashrafi <aref.ashrafi97@gmail.com>
2022-12-06 19:57:30 +0100

    polished

--- 2022-12-06/code_1.py
+++ 2022-12-06/code_1.py
@@ -1,15 +1,10 @@
-with open('input.txt') as f:
-    line = f.readline()
-    print(line)
-    count = 0
-    set_table = []
-    data = list(line)
-    for char in data:
-        set_table.extend(char)
-        if (len(set_table) > 14):
-            set_table.pop(0)
-        if (len(set_table) == 14 and len(set_table) == len(set(set_table))):
-            print(set_table)
-            print(count+1)
-            break
-        count += 1
+line = open('input.txt').readline()
+set_table = []
+data = list(line)
+for index, char in enumerate(data):
+    set_table.extend(char)
+    if (len(set_table) > 14):
+        set_table.pop(0)
+    if (len(set_table) == 14 and len(set_table) == len(set(set_table))):
+        print(index+1)
+        break
