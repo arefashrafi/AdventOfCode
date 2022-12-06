d00d054ec204abccc78458ff72361960a292428f
Aref Ashrafi <aref.ashrafi97@gmail.com>
2022-12-06 20:14:23 +0100

    polish

--- 2022-12-06/code_1.py
+++ 2022-12-06/code_1.py
@@ -1,9 +1,8 @@
+from collections import deque
 line = open('input.txt').readline()
-data_list = []
+data_list = deque([], maxlen=4)
 for index, char in enumerate(line):
     data_list.append(char)
-    if (len(data_list) > 4):
-        data_list.pop(0)
-    if (len(data_list) == len(set(data_list)) == 4):
+    if (len(data_list) == len(set(data_list)) == data_list.maxlen):
         print(index+1)
         break
